7bb51be034dc71d86026970f85a7ddace0d8757f
dima borisov <dim4ig2007@gmail.com>
2026-05-03 18:17:53 +0500

    Добавь анимации и скрипты на все двери

--- Assets/_Project/Runtime/Core/Props/Door.cs
+++ Assets/_Project/Runtime/Core/Props/Door.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Threading.Tasks;
 using _Project.Runtime.Interfaces;
 using UnityEngine;
 
@@ -8,20 +9,72 @@ namespace _Project.Runtime.Core.Props
     {
         [SerializeField] private Collider2D interactableCollider;
         
+        private static readonly int IsOpen = Animator.StringToHash("isOpen");
+        
         public SpriteRenderer Renderer { get; private set; }
+        
+        private Animator _animator; 
         private bool _isOpen;
         
         private void Awake()
         {
             Renderer = GetComponent<SpriteRenderer>();
+            _animator = GetComponent<Animator>();
             interactableCollider.isTrigger = false;
         }
 
-        public void Interact(GameObject initiator, Action onComplete)
+        public async void Interact(GameObject initiator, Action onComplete)
         {
-            _isOpen = !_isOpen;
-            interactableCollider.isTrigger = _isOpen;
             onComplete?.Invoke();
+            if (!_isOpen)
+                await SetDoorStateAsync(true);
+            else
+                if (!IsBlocked())
+                    await SetDoorStateAsync(false);
+        }
+
+        private bool IsBlocked()
+        {
+            var filter = new ContactFilter2D().NoFilter();
+            var results = new Collider2D[5];
+    
+            var count = interactableCollider.Overlap(filter, results);
+    
+            for (var i = 0; i < count; i++)
+                if (results[i].gameObject != gameObject) return true;
+
+            return false;
+        }
+
+        private async Task SetDoorStateAsync(bool open)
+        {
+            _animator.SetBool(IsOpen, open);
+
+            await Task.Yield(); 
+            var stateInfo = _animator.GetCurrentAnimatorStateInfo(0);
+
+            if (open) 
+            {
+                await Task.Delay(TimeSpan.FromSeconds(stateInfo.length * 0.4f));
+                _isOpen = true;
+                interactableCollider.isTrigger = true;
+            }
+            else 
+            {
+                await Task.Delay(TimeSpan.FromSeconds(stateInfo.length * 0.6f));
+
+                if (IsBlocked())
+                {
+                    _isOpen = true;
+                    interactableCollider.isTrigger = true;
+                    _animator.SetBool(IsOpen, true); 
+                }
+                else
+                {
+                    _isOpen = false;
+                    interactableCollider.isTrigger = false;
+                }
+            }
         }
 
         public bool IsInteractable => true;
